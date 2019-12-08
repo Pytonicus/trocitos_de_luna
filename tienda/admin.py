da563631e8ec18b0dbb007f8ad874b47a03df20b
Guillermo Granados Gómez <guillermogranadosgomez@gmail.com>
2019-12-08 12:51:43 +0100

    terminado los modelos de la tienda, falta revisar el carrito

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Cliente
+from .models import Cliente, Category, Material, Design, Product, Delivery, Cart, Billing
+
+class TiendaAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
 
 class ClienteAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
@@ -8,4 +11,26 @@ class ClienteAdmin(admin.ModelAdmin):
     search_fields = ('name', 'city', 'phone')
     list_filter = ('city', )
 
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('title', 'ref', 'price', 'category', 'material', 'design')
+    search_fields = ('title', 'ref', 'price', 'category__name', 'material__name', 'design__name')
+    list_filter = ('category__name', 'material__name', 'design__name', 'price')
+
+class DeliveryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('name', 'price', 'time')
+
+class BillingAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_buy', )
+    list_display = ('client', 'send', 'total', 'date_buy')
+
+
 admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Category, TiendaAdmin)
+admin.site.register(Material, TiendaAdmin)
+admin.site.register(Design, TiendaAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Delivery, DeliveryAdmin)
+admin.site.register(Cart)
+admin.site.register(Billing, BillingAdmin)
--- tienda/models.py
+++ tienda/models.py
@@ -72,14 +72,117 @@ class Cliente(models.Model):
     
     def __str__(self):
         return self.name
-# Categoria
 
-# Acabado
+class Category(models.Model):
+    name = models.CharField(max_length = 100, verbose_name = 'Nombre de la categoría')
+    image = models.ImageField(upload_to = 'categorias/', blank = True, verbose_name = 'Imagen de la categoría')
 
-# Diseño
+    created = models.DateTimeField(auto_now_add = True, verbose_name = 'Fecha de creación')
+    updated = models.DateTimeField(auto_now = True, verbose_name = 'Fecha de edición')
 
-# Productos
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Listado de Categorías'
+        ordering = ['name']
 
-# Carrito
+    def __str__(self):
+        return self.name
 
-# Compras
+class Material(models.Model):
+    name = models.CharField(max_length = 100, verbose_name = 'Tipo de material')
+    image = models.ImageField(upload_to = 'material/', blank = True, verbose_name = 'Imagen del material')
+
+    created = models.DateTimeField(auto_now_add = True, verbose_name = 'Fecha de creación')
+    updated = models.DateTimeField(auto_now = True, verbose_name = 'Fecha de edición')
+
+    class Meta:
+        verbose_name = 'Material'
+        verbose_name_plural = 'Listado de Materiales'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Design(models.Model):
+    name = models.CharField(max_length = 100, verbose_name = 'Diseño')
+    image = models.ImageField(upload_to = 'design/', blank = True, verbose_name = 'Imagen del diseño')
+
+    created = models.DateTimeField(auto_now_add = True, verbose_name = 'Fecha de creación')
+    updated = models.DateTimeField(auto_now = True, verbose_name = 'Fecha de edición')
+
+    class Meta:
+        verbose_name = 'Diseño'
+        verbose_name_plural = 'Listado de Diseños'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length = 200, verbose_name = 'Nombre del producto')
+    ref = models.IntegerField(verbose_name = 'Referencia')
+    image = models.ImageField(upload_to = 'product/', blank = True, verbose_name = 'Imagen del producto')
+    description = models.TextField(blank = True, verbose_name = 'Descripción del producto')
+    price = models.FloatField(verbose_name = 'Precio')
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, verbose_name = 'Categoría')
+    material = models.ForeignKey(Material, on_delete = models.CASCADE, verbose_name = 'Tipo de material')
+    design = models.ForeignKey(Design, on_delete = models.CASCADE, blank = True, null = True, verbose_name = 'Diseño')
+
+    created = models.DateTimeField(auto_now_add = True, verbose_name = 'Fecha de creación')
+    updated = models.DateTimeField(auto_now = True, verbose_name = 'Fecha de edición')
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Listado de Productos'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class Delivery(models.Model):
+    name = models.CharField(max_length = 150, verbose_name = 'Nombre de la empresa')
+    logo = models.ImageField(upload_to = 'delivery', blank = True, verbose_name = 'Logo de la empresa')
+    price = models.FloatField(verbose_name = 'Precio de envío')
+    time = models.IntegerField(verbose_name = 'Plazo de envío')
+
+    created = models.DateTimeField(auto_now_add = True, verbose_name = 'Fecha de creación')
+    updated = models.DateTimeField(auto_now = True, verbose_name = 'Fecha de edición')
+
+    class Meta:
+        verbose_name = 'Envio'
+        verbose_name_plural = 'Empresas de Envío'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    client = models.ForeignKey(Cliente, on_delete = models.CASCADE, verbose_name = 'Cliente')
+    product = models.ForeignKey(Product, on_delete = models.CASCADE, verbose_name = 'Producto')
+    lot = models.IntegerField(verbose_name = 'Cantidad')
+
+    created = models.DateTimeField(auto_now_add = True, verbose_name = 'Fecha de creación')
+    updated = models.DateTimeField(auto_now = True, verbose_name = 'Fecha de edición')
+
+    class Meta:
+        verbose_name = 'Carrito'
+        verbose_name_plural = 'Carrito de la compra'
+        ordering = ['client']
+
+    def __str__(self):
+        return self.client
+
+class Billing(models.Model):
+    client = models.CharField(max_length = 150, verbose_name = 'Cliente')
+    products = models.TextField(max_length = 250, verbose_name = 'Artículos')
+    send = models.CharField(max_length = 100, verbose_name = 'Método de Envío')
+    total = models.FloatField(verbose_name = 'Total')
+    date_buy = models.DateTimeField(auto_now_add = True, verbose_name = 'Fecha de pedido')
+
+    class Meta:
+        verbose_name = 'Factura'
+        verbose_name_plural = 'Facturas'
+        ordering = ['client']
+
+    def __str__(self):
+        return self.client
